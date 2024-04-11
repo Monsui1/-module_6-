0f4e9396653c707f731c829bf7e58a8dbe393efa
Monsui <159785429+Monsui1@users.noreply.github.com>
2024-04-12 01:13:09 +0300

    Add files via upload

--- ЗАДАНИЕ 6.6.1.js
+++ ЗАДАНИЕ 6.6.1.js
@@ -1,10 +1,9 @@
 //Пример 1
 
 function isPalindrome(Довод) {
-    Довод = Довод.toLowerCase();
     Довод = Довод.replace(/\s/g, '');
     
-    if (Довод === Довод.split('').reverse().join('')) {
+    if (Довод.toLowerCase() === Довод.toLowerCase().split('').reverse().join('')) {
         console.log(`Слово ${Довод} является палиндромом`);
     } else {
         console.log(`Слово ${Довод} не является палиндромом`);
@@ -17,13 +16,11 @@ isPalindrome(Довод);
 //Пример 2
 
 function isPalindrome(Сантимент) {
-    Сантимент = Сантимент.toLowerCase(); 
     Сантимент = Сантимент.replace(/\s/g, '');
     
-    if (Сантимент === Сантимент.split('').reverse().join('')) {
+    if (Сантимент.toLowerCase() === Сантимент.toLowerCase().split('').reverse().join('')) {
         console.log(`Слово ${Сантимент} является палиндромом`);
-    } 
-    else { 
+    } else {
         console.log(`Слово ${Сантимент} не является палиндромом`);
     }
 }
